4810c23cc3de19895743e8b8906ced90e3667158
Rita Pessoa <rita.pessoa.2002@gmail.com>
2023-05-25 16:11:35 +0100

    fix: wireframe e comments

--- js/main-script.js
+++ js/main-script.js
@@ -4,6 +4,8 @@ cameraFrontal, cameraLateral, cameraTopo, cameraOrtogonal, cameraPerspectiva,
 renderer, 
 activeCamera;
 
+var materials = []; // Array to store materials
+
 /* CREATE SCENE(S) */
 function createScene() {
     'use strict';
@@ -14,81 +16,83 @@ function createScene() {
 
     // ------------------------ROBOT-----------------------------------
     var top, bottom;
+    var blueMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+    var redMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    materials.push(blueMaterial); // Add material to materials array
+    materials.push(redMaterial); // Add material to materials array
+
+    // ---- PARTE DE CIMA ----
+    var top = new THREE.Object3D();
+
+    // ---- CABEÇA ----
+    var fullhead = new THREE.Object3D();
 
     //head (cube)
     var headGeometry = new THREE.BoxGeometry(80, 80, 80);
-    var headMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var head = new THREE.Mesh(headGeometry, headMaterial);
+    var head = new THREE.Mesh(headGeometry, blueMaterial);
     head.position.set(0,125,0);
-    
+    fullhead.add(head);
+
     //antenas (cylinder)
     var antenasGeometry = new THREE.CylinderGeometry(10,10,70);
-    var antenasMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    var antenas1 = new THREE.Mesh(antenasGeometry, antenasMaterial);
-    var antenas2 = new THREE.Mesh(antenasGeometry, antenasMaterial);
+    var antenas1 = new THREE.Mesh(antenasGeometry, redMaterial);
+    var antenas2 = new THREE.Mesh(antenasGeometry, redMaterial);
     antenas1.position.set(50,140,0);
     antenas2.position.set(-50,140,0);
+    fullhead.add(antenas1);
+    fullhead.add(antenas2);
+
+    top.add(fullhead);
+
+    // ---- TRONCO ----
 
     // tronco (cube)
     var troncoGeometry = new THREE.BoxGeometry(300, 150, 150);
-    var troncoMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    var tronco = new THREE.Mesh(troncoGeometry, troncoMaterial);
+    var tronco = new THREE.Mesh(troncoGeometry, redMaterial);
+    top.add(tronco);
 
     //abdomen (cube)
-    var abdomenGeometry = new THREE.BoxGeometry(150, 150, 50); //largura altura profundidade
-    var abdomenMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var abdomen = new THREE.Mesh(abdomenGeometry, abdomenMaterial);
+    var abdomenGeometry = new THREE.BoxGeometry(150, 100, 150); //largura altura profundidade
+    var abdomen = new THREE.Mesh(abdomenGeometry, blueMaterial);
     abdomen.position.set(0,-125,0);
-
-    //ADD TO TOP
-    var top = new THREE.Object3D();
-    top.add(head);
-    top.add(antenas1);
-    top.add(antenas2);
-    top.add(tronco);
     top.add(abdomen);
-    
 
-    var fullArm1, fullArm2;
+    // ---- BRAÇOS ----
+    var fullArm1 = new THREE.Object3D();
+    var fullArm2 = new THREE.Object3D();
+
     //arm1 (cube)
     var armGeometry = new THREE.BoxGeometry(100, 150, 100);
-    var armMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var arm1 = new THREE.Mesh(armGeometry, armMaterial);
+    var arm1 = new THREE.Mesh(armGeometry, blueMaterial);
     arm1.position.set(-200,0,-25);
+    fullArm1.add(arm1);
+
     //arm2 (cube)
-    var arm2 = new THREE.Mesh(armGeometry, armMaterial);
+    var arm2 = new THREE.Mesh(armGeometry, blueMaterial);
     arm2.position.set(200,0,-25); 
+    fullArm2.add(arm2);
 
     //forearm1 (cube)
     var forearmGeometry = new THREE.BoxGeometry(100, 100, 150);
-    var forearmMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    var forearm1 = new THREE.Mesh(forearmGeometry, forearmMaterial);
+    var forearm1 = new THREE.Mesh(forearmGeometry, redMaterial);
     forearm1.position.set(-200,-125,0);
+    fullArm1.add(forearm1)
+
     //forearm2 (cube)
-    var forearm2 = new THREE.Mesh(forearmGeometry, forearmMaterial);
+    var forearm2 = new THREE.Mesh(forearmGeometry, redMaterial);
     forearm2.position.set(200,-125,0);
+    fullArm2.add(forearm2);
 
     //tuboEscape1 (cylinder)
     var tuboEscapeGeometry = new THREE.CylinderGeometry(50, 50, 100);
-    var tuboEscapeMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var tuboEscape1 = new THREE.Mesh(tuboEscapeGeometry, tuboEscapeMaterial);
+    var tuboEscape1 = new THREE.Mesh(tuboEscapeGeometry, blueMaterial);
     tuboEscape1.position.set(-300,-125,-25);
+    fullArm1.add(tuboEscape1);
 
     //tuboEscape2 (cylinder)
     var tuboEscapeGeometry = new THREE.CylinderGeometry(50, 50, 100);
-    var tuboEscapeMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var tuboEscape2 = new THREE.Mesh(tuboEscapeGeometry, tuboEscapeMaterial);
+    var tuboEscape2 = new THREE.Mesh(tuboEscapeGeometry, blueMaterial);
     tuboEscape2.position.set(300,-125,-25);
-
-    fullArm1 = new THREE.Object3D();
-    fullArm2 = new THREE.Object3D();
-
-    fullArm1.add(arm1);
-    fullArm1.add(forearm1);
-    fullArm1.add(tuboEscape1);
-    
-    fullArm2.add(arm2);
-    fullArm2.add(forearm2);
     fullArm2.add(tuboEscape2);
 
     //ADD TO TOP
@@ -96,27 +100,25 @@ function createScene() {
     top.add(fullArm2);
     //TOP COMPLETE
 
-
+    
     // ---- PARTE DE BAIXO ---
     var fullWaist = new THREE.Object3D();
 
     //waist (cube)
-    var waistGeometry = new THREE.BoxGeometry(300, 100, 150);
-    var waistMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var waist = new THREE.Mesh(waistGeometry, waistMaterial);
+    var waistGeometry = new THREE.BoxGeometry(300, 100, 100);
+    var waist = new THREE.Mesh(waistGeometry, blueMaterial);
     waist.position.set(0,-225,0);
     fullWaist.add(waist);
 
     //wheel1 (cylinder)
     var wheelGeometry = new THREE.CylinderGeometry(50, 50, 50);
-    var wheelMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    var wheel1 = new THREE.Mesh(wheelGeometry, wheelMaterial);
+    var wheel1 = new THREE.Mesh(wheelGeometry, redMaterial);
     wheel1.rotation.z += 77;
     wheel1.position.set(-175,-225,0);
     fullWaist.add(wheel1);
 
     //wheel2 (cylinder)
-    var wheel2 = new THREE.Mesh(wheelGeometry, wheelMaterial);
+    var wheel2 = new THREE.Mesh(wheelGeometry, redMaterial);
     wheel2.rotation.z += 77;
     wheel2.position.set(175,-225,0);
     fullWaist.add(wheel2);
@@ -127,39 +129,36 @@ function createScene() {
 
     //coxa1 (cube)
     var coxaGeometry = new THREE.BoxGeometry(50, 200, 50);
-    var coxaMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-    var coxa1 = new THREE.Mesh(coxaGeometry, coxaMaterial);
+    var coxa1 = new THREE.Mesh(coxaGeometry, redMaterial);
     coxa1.position.set(-100,-375,0);
     fullLeg1.add(coxa1);
 
     //coxa2 (cube)
-    var coxa2 = new THREE.Mesh(coxaGeometry, coxaMaterial);
+    var coxa2 = new THREE.Mesh(coxaGeometry, redMaterial);
     coxa2.position.set(100,-375,0);
     fullLeg2.add(coxa2);
 
     //perna1 (cube)
     var pernaGeometry = new THREE.BoxGeometry(100, 200, 100);
-    var pernaMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-    var perna1 = new THREE.Mesh(pernaGeometry, pernaMaterial);
+    var perna1 = new THREE.Mesh(pernaGeometry, blueMaterial);
     perna1.position.set(-100,-575,0);
     fullLeg1.add(perna1);
 
     //perna2 (cube)
-    var perna2 = new THREE.Mesh(pernaGeometry, pernaMaterial);
+    var perna2 = new THREE.Mesh(pernaGeometry, blueMaterial);
     perna2.position.set(100,-575,0);
     fullLeg2.add(perna2);
 
     // ---- PES ----
 
     //foot1 (cube)
-    var footGeometry = new THREE.BoxGeometry(150, 50, 125);
-    var footMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000  });
-    var foot1 = new THREE.Mesh(footGeometry, footMaterial);
-    foot1.position.set(100,-675,12.5);
+    var footGeometry = new THREE.BoxGeometry(150, 25, 125);
+    var foot1 = new THREE.Mesh(footGeometry, redMaterial);
+    foot1.position.set(100,-687.5,12.5);
     fullLeg1.add(foot1);
     //foot2 (cube)
-    var foot2 = new THREE.Mesh(footGeometry, footMaterial);
-    foot2.position.set(-100,-675,12.5);
+    var foot2 = new THREE.Mesh(footGeometry, redMaterial);
+    foot2.position.set(-100,-687.5,12.5);
     fullLeg2.add(foot2);
 
     //ADD TO BOTTOM
@@ -314,46 +313,17 @@ function onKeyDown(e) {
         case 54: // Numeric key 6
             toggleWireframe();
             break;
-        case 81: // Letter key Q
-            if(keydown = 81){ //while ou if?? pq supostamente só mexe enquanto estamos a pressionar
-                 //();
-            }
-           
-            break;
-        case 65: // Letter key A
-            //();
-            break;
-        case 87: // Letter key W
-            //();
-            break;
-        case 83: // Letter key S
-            //();
-            break;
-        case 69: // Letter key E
-            //();
-            break;
-        case 68: // Letter key D
-            //();
-            break;
-        case 82: // Letter key R
-            //();
-            break;
-        case 70: // Letter key F
-            //();
-            break;
         default:
             break;
     }
 }
 
 function toggleWireframe() {
-    // Toggle wireframe mode for all objects in the scene //MUDAR, GUARDAR ARRAY GLOBAL MATERIAIS, WIREFRAME TRUE
-    scene.traverse(function (object) {
-      if (object instanceof THREE.Mesh) {
-        object.material.wireframe = true;
-      }
-    });
-  }
+    for (var i = 0; i < materials.length; i++) {
+        var material = materials[i];
+        material.wireframe = !material.wireframe;
+    }
+}
 
 /* ADD EVENT LISTENERS */
 window.addEventListener('resize', onResize, false);
